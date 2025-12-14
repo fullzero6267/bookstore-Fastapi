31fc1dfa776dc6a71b1e6ab31ffab35bc760c81e
Nemo <cycy98@naver.com>
2025-12-14 22:58:30 +0900

    api 라우터 수정

--- src/app/main.py
+++ src/app/main.py
@@ -13,11 +13,11 @@ from app.api.routes import Auth, Users, Books, Carts, Orders, Favorites, Reviews
 # title은 Swagger(/docs)에 표시될 API 이름
 app = FastAPI(title="Bookstore API Chan")
 
-#api
+#api prefix(경로) 고정
 app.include_router(Auth.router, prefix="/api/auth", tags=["Auth"])
 app.include_router(Users.router, prefix="/api/users", tags=["Users"])
-app.include_router(Books.router, prefix="/api", tags=["Books"])      # /api/books, /api/public/books
+app.include_router(Books.router, prefix="/api/books", tags=["Books"])      # /api/books, /api/public/books
 app.include_router(Carts.router, prefix="/api/carts", tags=["Carts"])
 app.include_router(Orders.router, prefix="/api/orders", tags=["Orders"])
 app.include_router(Favorites.router, prefix="/api/favorites", tags=["Favorites"])
-app.include_router(Reviews.router, prefix="/api", tags=["Reviews"])  # /api/books/{bookId}/reviews, /api/reviews/...
+app.include_router(Reviews.router, prefix="/api/reviews", tags=["Reviews"])  # /api/books/{bookId}/reviews, /api/reviews/...
